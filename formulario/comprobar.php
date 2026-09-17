41c4c4fa4501e7208f5312b138c64076b128084d
David rodriguez <Davidro08@educastur.es>
2026-09-17 14:16:36 +0200

    Mostrar usuario y contraseña con echo

--- formulario/comprobar.php
+++ formulario/comprobar.php
@@ -4,4 +4,13 @@ $n = $_GET ['usuario'];
 $p = $_GET ['password'];   
 echo $n;
 echo $p;
+$USU='david';
+$CONTRA='1234';
+if ($n==$USU && $p==$CONTRA){
+    echo "<p>Bienvenido $n</p>";
+}
+else{
+    echo "<p>Usuario o contraseña incorrectos</p>";
+}
+
 ?>
