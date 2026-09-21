535446b4a108f60b8d64b2b841661227f660409d
David Rodriguez <davidro08@educastur.es>
2026-09-21 12:21:18 +0200

    notas

--- Calculadora en un fichero/calculadora.php
+++ Calculadora en un fichero/calculadora.php
@@ -1,43 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="Calculadora" />
-    <title>Calculadora</title>
-  </head>
-  <body>
-    <form action="" method="POST"> <!-- si se hace dentro del mismo fichero se pone "" donde el action -->
-      <label>num 1:</label>
-      <input type="number" name="num1" required />
+<?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
+  <?php
+  $num1 = $_POST['num1'];
+  $num2 = $_POST['num2'];
+  $opera = $_POST['opera'];
 
-      <label>num 2:</label>
-      <input type="number" name="num2" required />
+  $resultado = match($opera) {
+      '+' => $num1 + $num2,
+      '-' => $num1 - $num2,
+      '*' => $num1 * $num2,
+      '/' => $num2 != 0 ? $num1 / $num2 : 'Error: No se puede dividir entre cero',
+      default => 'Operación inválida',
+  };
 
-      <label>opera:</label>
-      <select name="opera">
-        <option value="*">*</option>
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value="/">/</option>
-      </select>
-      <br /><br />
-      <button type="submit">Calcula</button>
-    </form>
-  </body>
-</html>
+  echo "El resultado de la operación es: $resultado";
+  ?>
+  <br><br>
+  <a href="">Volver a calcular</a>
 
-<?php
-$num1 = $_POST['num1'];
-$num2 = $_POST['num2'];
-$opera = $_POST['opera'];
+<?php else: ?>
 
-$resultado = match($opera) {
-    '+' => $num1 + $num2,
-    '-' => $num1 - $num2,
-    '*' => $num1 * $num2,
-    '/' => $num2 != 0 ? $num1 / $num2 : 'Error: No se puede dividir entre cero',
-    default => 'Operación inválida',
-};
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="Calculadora" />
+      <title>Calculadora</title>
+    </head>
+    <body>
+      <form action="" method="POST"> <!-- si se hace dentro del mismo fichero se pone "" donde el action -->
+        <label>num 1:</label>
+        <input type="number" name="num1" required />
 
-echo "El resultado de la operación es: $resultado";
-?>
+        <label>num 2:</label>
+        <input type="number" name="num2" required />
+
+        <label>opera:</label>
+        <select name="opera">
+          <option value="*">*</option>
+          <option value="+">+</option>
+          <option value="-">-</option>
+          <option value="/">/</option>
+        </select>
+        <br /><br />
+        <button type="submit">Calcula</button>
+      </form>
+    </body>
+  </html>
+
+<?php endif; ?>
